818e4586ec53de4fdd9c1e95e5102cb91f7d23b5
Alexander Sapozhnikov <shoorick@mail.ru>
2010-07-01 15:53:05 +0600

    Fixed newlines in initialization script. Added field "text" into table "user".

--- init.sql
+++ init.sql
@@ -1,9 +1,14 @@
 BEGIN TRANSACTION;
 CREATE TABLE "page" ("id" INTEGER PRIMARY KEY AUTOINCREMENT, "title" TEXT, "text" TEXT);
-INSERT INTO "page" VALUES(1,'Index','First page\n\nLightweight wiki *mojowka* based on top of "Mojolicious::Lite":http://search.cpan.org/dist/Mojolicious\n\n"Registered user":/login can change content of pages.');
-INSERT INTO "page" VALUES(2,'Sitemap','* "First page of site":/\n* "This page":/sitemap');
-CREATE TABLE "user" ("id" INTEGER PRIMARY KEY AUTOINCREMENT, "login" TEXT, "password" TEXT);
-INSERT INTO "user" VALUES(1,'admin','21232f297a57a5a743894a0e4a801fc3');
+INSERT INTO "page" VALUES(1,'Index','First page
+
+Lightweight wiki *mojowka* based on top of "Mojolicious::Lite":http://search.cpan.org/dist/Mojolicious
+
+"Registered user":/login can change content of pages.');
+INSERT INTO "page" VALUES(2,'Sitemap','* "First page of site":/
+* "This page":/sitemap');
+CREATE TABLE "user" ("id" INTEGER PRIMARY KEY AUTOINCREMENT, "login" TEXT, "password" TEXT, "text" TEXT);
+INSERT INTO "user" VALUES(1,'admin','21232f297a57a5a743894a0e4a801fc3','Administrator of site. He can edit pages.');
 CREATE UNIQUE INDEX "page_title" on page (title ASC);
 CREATE UNIQUE INDEX "user_login" on user (login ASC);
 COMMIT;
