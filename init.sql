f309cacab2ebc76fafda59db1887154d1442f99d
Alexander Sapozhnikov <shoorick@mail.ru>
2010-07-22 01:35:21 +0600

    Removed unused Sitemap page from initial database. Changed text on main page.

--- init.sql
+++ init.sql
@@ -2,11 +2,11 @@ BEGIN TRANSACTION;
 CREATE TABLE "page" ("id" INTEGER PRIMARY KEY AUTOINCREMENT, "title" TEXT, "text" TEXT);
 INSERT INTO "page" VALUES(1,'','First page
 
-Lightweight wiki *mojowka* based on top of "Mojolicious::Lite":http://search.cpan.org/dist/Mojolicious
+"Mojolicious::Lite":http://search.cpan.org/dist/Mojolicious based lightweight wiki *mojowka*.
 
-"Registered user":/login can change content of pages.');
-INSERT INTO "page" VALUES(2,'Sitemap','* "First page of site":/
-* "This page":/sitemap');
+"Registered user":/login can change content of pages or create new ones. Pages can contain any unicode characters. For example, you can write in Russian (да, вы можете писать по-русски).
+
+See also "sitemap":/sitemap and "search":/search pages.');
 CREATE TABLE "user" ("id" INTEGER PRIMARY KEY AUTOINCREMENT, "login" TEXT, "password" TEXT, "text" TEXT);
 INSERT INTO "user" VALUES(1,'admin','21232f297a57a5a743894a0e4a801fc3','Administrator of site. He can edit pages.');
 CREATE UNIQUE INDEX "page_title" on page (title ASC);
